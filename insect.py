9d46dad7553fcb878fe342167d2c884ff14e7481
samhuff98 <sam@hufflett.com>
2021-06-07 14:44:43 +0100

    New camera setup

--- insect.py
+++ insect.py
@@ -3,6 +3,7 @@ import numpy as np
 import subprocess
 
 cap = cv2.VideoCapture(1)
+cap.set(28, 20)
 
 # - Initialization -------------------------------------------
 
@@ -16,7 +17,7 @@ ticcmd('--max-decel', str(int(1500000)))
 ticcmd('--max-speed', str(int(18000000)))
 ticcmd('--current', str(int(320)))
 
-base_speed = 8000000
+base_speed = 6000000
 diff = 20000
 
 #set target area
@@ -29,26 +30,30 @@ while True:
     speed = 0
 
     ret, frame = cap.read()
-    grayscaled = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    retval, threshold = cv2.threshold(grayscaled, 120, 255, cv2.THRESH_BINARY)
+    #grayscaled = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #retval, threshold = cv2.threshold(grayscaled, 120, 255, cv2.THRESH_BINARY)
+    #res = cv2.bitwise_and(frame, frame, mask=threshold)
 
-    res = cv2.bitwise_and(frame, frame, mask=threshold)
+    #lower_insect = np.array([100, 100, 100])
+    #upper_insect = np.array([200, 200, 200])
+    #hsv1 = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #mask1 = cv2.inRange(hsv1, lower_insect, upper_insect)
 
-    lower_insect = np.array([100, 100, 100])
-    upper_insect = np.array([200, 200, 200])
-    hsv1 = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask1 = cv2.inRange(hsv1, lower_insect, upper_insect)
+    mask1 = np.ones(frame.shape, dtype=np.uint8)
+    mask1.fill(255)
+    roi_corners = np.array([[(100, -40), (100, 480), (610, 330), (610,150)]], dtype=np.int32)
+    cv2.fillPoly(mask1, roi_corners, 0)
+    cut_frame = cv2.bitwise_or(frame, mask1)
 
-    mask = np.zeros(frame.shape, dtype=np.uint8)
-    roi_corners = np.array([[(0, 0), (10, 300), (600, 300), (300,10)]], dtype=np.int32)
-    # fill the ROI so it doesn't get wiped out when the mask is applied
-    channel_count = frame.shape[2]  # i.e. 3 or 4 depending on your image
-    ignore_mask_color = (255,) * channel_count
-    cv2.fillPoly(mask, roi_corners, ignore_mask_color)
-    masked_image = cv2.bitwise_and(frame, mask)
+    grayscaled = cv2.cvtColor(cut_frame, cv2.COLOR_BGR2GRAY)
+    retval, threshold = cv2.threshold(grayscaled, 40, 255, cv2.THRESH_BINARY_INV)
 
+    #lower_insect = np.array([30, 20, 20])
+    #upper_insect = np.array([200, 200, 200])
 
-    #mask1 = cv2.inrange(hsv, lower_base, upper_base)
+    #hsv = cv2.cvtColor(cut_frame, cv2.COLOR_BGR2HSV)
+    #mask = cv2.inRange(hsv, lower_insect, upper_insect)
+    #res = cv2.bitwise_and(cut_frame, cut_frame, mask=mask)
     #retval, threshold = cv2.threshold(grayscaled, 120, 255, cv2.THRESH_BINARY)
     #cropped = cv2.bitwise_and(frame, frame, mask=mask1)
     #crop_frame = frame[TA[2]:TA[3], TA[0]:TA[1]]
@@ -71,11 +76,11 @@ while True:
     #erosion = cv2.erode(mask, kernel, iterations=1)
     #dilation = cv2.dilate(erosion, kernel, iterations = 1)
 
-    cnts = cv2.findContours(mask1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:10]
 
-    minimum_area = 20
+    minimum_area = 1000
     for c in cnts:
 
         area = cv2.contourArea(c)
@@ -84,15 +89,18 @@ while True:
             M = cv2.moments(c)
             cX = int(M["m10"] / M["m00"])
             cY = int(M["m01"] / M["m00"])
-            cv2.circle(frame, (cX + TA[0], cY + TA[2]), 20, (36, 255, 12), 2)
+            cv2.circle(frame, (cX, cY), 20, (36, 255, 12), 2)
             x, y, w, h = cv2.boundingRect(c)
             x_value = x
             cv2.putText(frame, 'X value: {}'.format(x), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36, 255, 12), 2)
             cv2.putText(frame, 'Y value: {}'.format(y), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36, 255, 12), 2)
             break
 
-    if 5 < x_value < (TA[1]-TA[0]-30):
-        speed = base_speed +((195 - x_value) * diff)
+    if 100 < x_value < 300:
+        speed = base_speed +((300-x_value) * diff)
+
+    elif 300<= x_value < 550:
+        speed = base_speed
     else:
         speed = 0
 
@@ -100,10 +108,10 @@ while True:
     ticcmd('--exit-safe-start', '--velocity', str(speed))
 
     cv2.imshow('frame', frame)
-    cv2.imshow('res', res)
-    cv2.imshow('mask', mask1)
+    #cv2.imshow('hsv', hsv)
+    #cv2.imshow('mask', mask)
     cv2.imshow('threshold', threshold)
-    cv2.imshow('crop', masked_image)
+    cv2.imshow('cut_frame', cut_frame)
     #cv2.imshow('dilation', dilation)
     #cv2.imshow('blur', blur)
 
