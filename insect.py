3aa08baa5d08548b3b304d892bd840e6ba7b9990
samhuff98 <sam@hufflett.com>
2021-06-09 11:45:20 +0100

    Adjusted in lab

--- insect.py
+++ insect.py
@@ -17,13 +17,12 @@ ticcmd('--max-decel', str(int(1500000)))
 ticcmd('--max-speed', str(int(18000000)))
 ticcmd('--current', str(int(320)))
 
-base_speed = 6000000
+base_speed = 5000000
 diff = 20000
 
 #set target area
-TA = [110, 500, 225, 365]
-TA_centre = [((TA[1]-TA[0])/2), ((TA[3]-TA[2])/2)]
-print(TA_centre)
+# TA = [110, 500, 225, 365]
+# TA_centre = [((TA[1]-TA[0])/2), ((TA[3]-TA[2])/2)]
 
 while True:
     x_value = 0
@@ -33,7 +32,6 @@ while True:
     #grayscaled = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     #retval, threshold = cv2.threshold(grayscaled, 120, 255, cv2.THRESH_BINARY)
     #res = cv2.bitwise_and(frame, frame, mask=threshold)
-
     #lower_insect = np.array([100, 100, 100])
     #upper_insect = np.array([200, 200, 200])
     #hsv1 = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -46,11 +44,10 @@ while True:
     cut_frame = cv2.bitwise_or(frame, mask1)
 
     grayscaled = cv2.cvtColor(cut_frame, cv2.COLOR_BGR2GRAY)
-    retval, threshold = cv2.threshold(grayscaled, 40, 255, cv2.THRESH_BINARY_INV)
+    retval, threshold = cv2.threshold(grayscaled, 80, 255, cv2.THRESH_BINARY_INV)
 
     #lower_insect = np.array([30, 20, 20])
     #upper_insect = np.array([200, 200, 200])
-
     #hsv = cv2.cvtColor(cut_frame, cv2.COLOR_BGR2HSV)
     #mask = cv2.inRange(hsv, lower_insect, upper_insect)
     #res = cv2.bitwise_and(cut_frame, cut_frame, mask=mask)
@@ -59,19 +56,14 @@ while True:
     #crop_frame = frame[TA[2]:TA[3], TA[0]:TA[1]]
     #hsv = cv2.cvtColor(crop_frame, cv2.COLOR_BGR2HSV)
     #output = cv2.rectangle(frame, (TA[0], TA[2]), (TA[1], TA[3]), (255, 0, 0), 2)
-
     #lower_red = np.array([30, 30, 130])
     #upper_red = np.array([105, 255, 255])
-
     #mask = cv2.inRange(hsv, lower_red, upper_red)
     #res = cv2.bitwise_and(crop_frame, crop_frame, mask=mask)
-
-
     #invert = cv2.bitwise_not(crop_frame)
     #gray = cv2.cvtColor(invert, cv2.COLOR_BGR2GRAY)
     #blur = cv2.medianBlur(gray, 25)
     #threshold, res = cv2.threshold(gray, 170, 255, cv2.THRESH_BINARY)
-
     #kernel = np.ones((5,5), np.uint8)
     #erosion = cv2.erode(mask, kernel, iterations=1)
     #dilation = cv2.dilate(erosion, kernel, iterations = 1)
@@ -80,11 +72,12 @@ while True:
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:10]
 
-    minimum_area = 300
+    minimum_area = 50
+    maximum_area = 1000
     for c in cnts:
 
         area = cv2.contourArea(c)
-        if (area > minimum_area):
+        if maximum_area > area > minimum_area:
             # Find centroid
             M = cv2.moments(c)
             cX = int(M["m10"] / M["m00"])
@@ -99,7 +92,7 @@ while True:
     if 0 < x_value < 300:
         speed = base_speed +((300-x_value) * diff)
 
-    elif 300<= x_value < 550:
+    elif 300 <= x_value < 550:
         speed = base_speed
     else:
         speed = 0
@@ -122,5 +115,3 @@ ticcmd('--enter-safe-start')
 ticcmd('--deenergize')
 cap.release()
 cv2.destroyAllWindows()
-
-
